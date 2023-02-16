27d065458ff1fa75b62d19181c12283f742258fa
Shourav Saha <shourav939@gmail.com>
2023-02-16 23:30:29 +0600

    i can added templete and modify more things

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,10 @@ use App\Models\User;
 class HomeController extends Controller
 {
     //
+    public function index(){
+        return view('home.userpage');
+    }
+
     public function redirect(){
         $usertype = Auth::user()->usertype;
         if($usertype=="1"){
