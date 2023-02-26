7c0c70608d12d8abb80bed602cf795e0eef9ee5c
Shourav Saha <shourav939@gmail.com>
2023-02-27 03:34:06 +0600

    show product data in userpage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        return view('home.userpage');
+        $product = Product::paginate(10);
+        return view('home.userpage', compact('product'));
     }
 
     public function redirect(){
@@ -18,7 +20,8 @@ class HomeController extends Controller
         if($usertype=="1"){
             return view('admin.home');
         }else{
-            return view('home.userpage');
+            $product = Product::paginate(10);
+            return view('home.userpage', compact('product'));
         }
     }
 }
